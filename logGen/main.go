0a52166a3022ffad6934573903f29f325917c56a
Oleh Bozhok <olehbozhok@gmail.com>
2019-08-03 22:50:46 +0300

    add some text to show indent debug

--- logGen/main.go
+++ logGen/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+
 	log "github.com/sirupsen/logrus"
 )
+
+type SomeType struct {
+	Field1 string
+	Field2 int
+}
 
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
@@ -20,5 +27,17 @@ func main() {
 	looked up one of the more obscure Latin words, consectetur, from a Lorem 
 	Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
    `)
+
+	st := SomeType{"text1", 2}
+	b, _ := json.Marshal(st)
+	log.WithField("q", st).Info("some text")
+	log.WithField("q", string(b)).Info("some text")
+
+	st2 := make(map[string]interface{})
+	err := json.Unmarshal(b, &st2)
+	if err != nil {
+		panic(err)
+	}
+	log.WithField("st", st).Info("Unmarshaled to struct")
 
 }
